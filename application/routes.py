283912f6ec9b67fed96714c2f6b6bec9a6ea58b4
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-19 20:05:03 +0800

    add sklearn

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,10 @@
 from application import app
 from flask import render_template, request
 import json
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+
+clf = pickle.load(open('./gender_classifier.pkl', 'rb'))
 
 @app.route('/', methods=['GET'])
 def index():
@@ -8,5 +12,12 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-  name = request.form['name']
+  cv = CountVectorizer()
+  names = []
+  names.append(request.form['name'])
+  print(names)
+  # vector = cv.transform(names)
+  # print(vector)
+  # prediction = clf.predict(vector)
+  # print(prediction)
   return render_template('index.html')
